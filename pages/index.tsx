49f249177ee80b99aff9d5e7b60a5f5ee43b1204
JunSeong <gentlmanjun@naver.com>
2022-11-28 09:36:33 +0900

    feat | add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
         <title>배준성 포트폴리오</title>
         <meta name="keyword" content="포트폴리오, 프론트엔드" />
         <meta name="description" content="배준성 포트폴리오" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
     </>
   );
