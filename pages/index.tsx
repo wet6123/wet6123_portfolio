c5e678797c8af0919030d91300ed4db0a0646b22
JunSeong <gentlmanjun@naver.com>
2022-11-28 09:35:06 +0900

    feat | set title and meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Hi Next.js</h1>;
+  return (
+    <>
+      <Head>
+        <title>배준성 포트폴리오</title>
+        <meta name="keyword" content="포트폴리오, 프론트엔드" />
+        <meta name="description" content="배준성 포트폴리오" />
+      </Head>
+    </>
+  );
 }
