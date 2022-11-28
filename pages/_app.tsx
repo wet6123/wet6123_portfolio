96b7a2fa8bf5f66ae4b499bbc837edaeeff0c4ad
JunSeong <gentlmanjun@naver.com>
2022-11-28 16:54:58 +0900

    feat | add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "../public/static/fonts/style.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
