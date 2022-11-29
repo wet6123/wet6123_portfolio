0aca1294e6ef1adbc896a196bfbda27ec3d3e9e9
JunSeong <gentlmanjun@naver.com>
2022-11-29 12:44:55 +0900

    feat | Impl management skills

--- components/home/skills.tsx
+++ components/home/skills.tsx
@@ -4,6 +4,8 @@ import JsTsLogo from "./../../public/front/JS_TS_logo.png";
 import HtmlCssLogo from "./../../public/front/HTML_CSS_logo.png";
 import Spring from "./../../public/back/Spring_logo.png";
 import MySQL from "./../../public/back/Mysql_logo.png";
+import Jira from "./../../public/manage/Jira_logo.png";
+import Git from "./../../public/manage/Git_logo.png";
 
 export default function Skills() {
   return (
@@ -60,7 +62,7 @@ export default function Skills() {
       백엔드
       
       */}
-      <div className="flex flex-col text-center w-full mb-5">
+      <div className="flex flex-col text-center w-full mb-5 mt-10">
         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
           Back-End
         </h1>
@@ -69,7 +71,7 @@ export default function Skills() {
         <SkillTile
           name="Spring"
           img={Spring}
-          rate={3}
+          rate={2}
           des={[
             "- Spring (Boot, Core)를 활용하여 서버를 구축할 수 있습니다.",
             "- JPA를 사용하여 서버를 개발한 경험이 있습니다.",
@@ -83,6 +85,36 @@ export default function Skills() {
           des={[
             "- MySQL을 프로젝트에 사용한 경험이 있습니다.",
             "- 프로젝트에 필요한 DB를 설계하고 테스트에 필요한 데이터을 관리한 경험이 있습니다.",
+          ]}
+        />
+      </div>
+      {/* 
+      
+      협업관리툴
+      
+      */}
+      <div className="flex flex-col text-center w-full mb-5 mt-10">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+          Project management tool
+        </h1>
+      </div>
+      <div className="flex flex-wrap -m-4">
+        <SkillTile
+          name="Jira"
+          img={Jira}
+          rate={3}
+          des={[
+            "- 팀장으로서 Jira를 활용하여 프로젝트를 관리한 경험이 있습니다.",
+            "- 애자일과 스크럼을 통해서 프로젝트 진행하였습니다.",
+          ]}
+        />
+        <SkillTile
+          name="Git"
+          img={Git}
+          rate={4}
+          des={[
+            "- Git을 이용하여 버전을 관리하고 github나 gitlab의 원격저장소를 활용합니다.",
+            "- 협업을 위해서 커밋 컨벤션을 설정하고 Git-Flow전략을 활용했습니다.",
           ]}
         />
       </div>
