7ad6623d2fbd5ad78bdf12eaf7722390c1976702
JunSeong <gentlmanjun@naver.com>
2022-11-29 11:10:30 +0900

    feat | Impl about me

--- components/home/aboutMe.tsx
+++ components/home/aboutMe.tsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-gray-900">
               ABOUT ME
             </h1>
           </div>
