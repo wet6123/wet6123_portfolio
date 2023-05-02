19078f81fbe855ed5dfd21e461e4e6b4009977ef
JunSeong <gentlmanjun@naver.com>
2023-05-02 15:59:19 +0900

    feat: 폰트 추가, aboutMe 폰트 변경

--- components/home/aboutMe.tsx
+++ components/home/aboutMe.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
+import styles from "styles/aboutme.module.scss";
 
 export default function AboutMe() {
   return (
@@ -7,7 +7,7 @@ export default function AboutMe() {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-gray-900">
+            <h1 className="sm:text-5xl text-3xl font-bold title-font mb-4 text-gray-900">
               ABOUT ME
             </h1>
           </div>
@@ -22,8 +22,8 @@ export default function AboutMe() {
                   height={64}
                 />
                 <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">이름</h2>
-                  <p className="text-gray-500">배준성</p>
+                  <h2 className={styles.title}>이름</h2>
+                  <p className={styles.content}>배준성</p>
                 </div>
               </div>
             </div>
@@ -37,10 +37,8 @@ export default function AboutMe() {
                   height={64}
                 />
                 <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">
-                    생년월일
-                  </h2>
-                  <p className="text-gray-500">1996. 06. 23</p>
+                  <h2 className={styles.title}>생년월일</h2>
+                  <p className={styles.content}>1996. 06. 23</p>
                 </div>
               </div>
             </div>
@@ -54,9 +52,9 @@ export default function AboutMe() {
                   height={64}
                 />
                 <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">학력</h2>
-                  <p className="text-gray-500">한국외국어대학교</p>
-                  <p className="text-gray-500">
+                  <h2 className={styles.title}>학력</h2>
+                  <p className={styles.content}>한국외국어대학교</p>
+                  <p className={styles.content}>
                     전자공학 | 컴퓨터·전자시스템공학
                   </p>
                 </div>
@@ -72,10 +70,8 @@ export default function AboutMe() {
                   height={64}
                 />
                 <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">
-                    희망 직무
-                  </h2>
-                  <p className="text-gray-500">프론트엔드</p>
+                  <h2 className={styles.title}>희망 직무</h2>
+                  <p className={styles.content}>프론트엔드</p>
                 </div>
               </div>
             </div>
@@ -89,10 +85,8 @@ export default function AboutMe() {
                   height={64}
                 />
                 <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">
-                    이메일
-                  </h2>
-                  <p className="text-gray-500">gentlmanjun@naver.com</p>
+                  <h2 className={styles.title}>이메일</h2>
+                  <p className={styles.content}>gentlmanjun@naver.com</p>
                 </div>
               </div>
             </div>
@@ -106,14 +100,12 @@ export default function AboutMe() {
                   height={64}
                 />
                 <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">
-                    페이지가 보이지 않으신가요?
-                  </h2>
+                  <h2 className={styles.title}>페이지가 보이지 않으시나요?</h2>
                   <a
                     target="_blank"
                     href="https://mysterious-cesium-158.notion.site/PORTFOLIO-9ae6a50c69d94d92a50c6be27e751e92"
                   >
-                    <p className="text-gray-500">노션으로 보기</p>
+                    <p className={styles.content}>노션으로 보기</p>
                   </a>
                 </div>
               </div>
