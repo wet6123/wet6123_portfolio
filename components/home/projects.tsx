8049bc767a369dd53c19137231c9b7813e1524ad
JunSeong <gentlmanjun@naver.com>
2023-05-02 17:53:57 +0900

    feat: 프로젝트 추가

--- components/home/projects.tsx
+++ components/home/projects.tsx
@@ -1,5 +1,84 @@
-import Image from "next/image";
-import PjtTile from "../pjtTile";
+import { PjtTile } from "../pjtTile";
+
+const pjtData = [
+  {
+    id: 1,
+    img: "/img/pjt/intagral.gif",
+    titles: "InTagra∫ (인테그랄) - 삼성 SDS 연계 프로젝트",
+    semiTitle: "AI로 사진을 분석해 태그 달기를 도와주는 SNS 앱",
+    date: "2022.09.19 ~ 2022.11.21",
+    team: "6명 (백엔드 2, 안드로이드 2, AI 2)",
+    part: "백엔드",
+    contribute: 30,
+    skill: ["Spring Boot", "Kotlin", "MySQL", "YOLOv5"],
+    link: "https://github.com/wet6123/Intagral",
+    division: "SSAFY 2학기 자율 프로젝트",
+    list: [
+      "ERD 작성, DB 구성",
+      "이미지 업로드를 위한 AWS S3 Bucket 구성",
+      "Post 업로드, 삭제, 조회, 좋아요를 위한 API 구현",
+      "앱 UI / UX 개선 및 디자인",
+      "Notion 협업 공간 관리",
+    ],
+  },
+  {
+    id: 2,
+    img: "/img/pjt/masterpiece.gif",
+    titles: "Masterpiece (마스터피스) - 우수 프로젝트 선정 (서울 5반 1등)",
+    semiTitle: "외국인이 한글의 과학성과 예술성을 경험할 수 있는 NFT 플랫폼",
+    date: "2022.08.22 ~ 2022.09.16",
+    team: "5명 (프론트엔드 3, 백엔드 2)",
+    part: "팀장, 프론트엔드",
+    contribute: 40,
+    skill: [
+      "Spring Boot",
+      "React",
+      "Redux-Toolkit",
+      "Typescript",
+      "MySQL",
+      "Web3.js",
+      "Meta Mask",
+    ],
+    link: "https://github.com/wet6123/rnasterpiece",
+    division: "SSAFY 2학기 특화 프로젝트",
+    list: [
+      "redux-toolkit & react-router-dom / styled-components / ESLint & Prettier 사용",
+      "Typescript 적용",
+      "로그인, 마이페이지 구현",
+      "NFT 목록, 상세, 거래 페이지 구현",
+      "한글 게임, 뽑기 페이지 구현",
+      "한글 글자 꾸미기 구현 참여",
+      "Notion 협업 공간 관리",
+    ],
+  },
+  {
+    id: 3,
+    img: "/img/pjt/wetness.gif",
+    titles: "WE-TNESS (위트니스)",
+    semiTitle: "WebRTC와 모션인식을 활용한 운동 게임 서비스",
+    date: "2022.07.05 ~ 2022.08.19",
+    team: "6명 (프론트엔드 2, 백엔드 4, 백엔드 1명 중도 참여)",
+    part: "프론트엔드",
+    contribute: 25,
+    skill: [
+      "Spring Boot",
+      "React",
+      "Redux-Toolkit",
+      "MySQL",
+      "Web RTC",
+      "Teachable Machine",
+    ],
+    link: "https://github.com/wet6123/WE-tness",
+    division: "SSAFY 2학기 공통 프로젝트",
+    list: [
+      "react-toolkit & react-router-dom 활용 SPA 구현",
+      "styled-components 활용 css 스타일링",
+      "협업과 코드 리팩토링을 위한 ESLint 설정",
+      "메인 페이지, 튜토리얼, 유저 정보, 검색 구현",
+      "운동별 애니메이션 제작",
+    ],
+  },
+];
 
 export default function Projects() {
   return (
@@ -9,7 +88,7 @@ export default function Projects() {
           <h1 className="sm:text-5xl text-3xl font-bold title-font mb-4 text-gray-900">
             Projects
           </h1>
-          <PjtTile />
+          {pjtData && pjtData.map((pjt) => <PjtTile item={pjt} key={pjt.id} />)}
         </div>
       </div>
     </>
---
+++ components/pjtTile.tsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import { Badge } from "./project/badge";
+import styles from "styles/pjt.module.scss";
+
+export const PjtTile = (pjt: {
+  item: {
+    id: any;
+    img: any;
+    titles: any;
+    semiTitle: any;
+    date: any;
+    team: any;
+    part: any;
+    contribute: any;
+    skill: any;
+    link: any;
+    division: any;
+    list: any;
+  };
+}) => {
+  const {
+    id,
+    img,
+    titles,
+    semiTitle,
+    date,
+    team,
+    part,
+    contribute,
+    skill,
+    link,
+    division,
+    list,
+  } = pjt.item;
+  return (
+    <>
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+            <Image
+              className="object-cover object-center rounded"
+              alt="pjt img"
+              src={img}
+              width={720}
+              height={600}
+            />
+          </div>
+          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+            <h1 className="title-font sm:text-2xl text-lg mb-4 font-bold text-gray-900">
+              {titles}
+            </h1>
+            <p className={styles.semititle}>{semiTitle}</p>
+            <div className={styles.content}>
+              <p>개발 기간: {date}</p>
+              <p>개발 인원: {team}</p>
+              <p>담당 분야: {part}</p>
+              <p>기여도: {contribute}%</p>
+            </div>
+            <p className="my-2">
+              {skill && skill.map((stack: any) => <Badge item={stack} />)}
+            </p>
+            <a target="_blank" href={link}>
+              <button className="mt-2 mb-4 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                자세히보기
+              </button>
+            </a>
+            <p className={styles.title}>{division}</p>
+            <br />
+            {list ? (
+              <>
+                <div className={styles.content}>담당분야</div>
+                {list &&
+                  list.map((stack: any) => (
+                    <li className={styles.content} key={`${id} + ${stack}`}>
+                      {stack}
+                    </li>
+                  ))}
+              </>
+            ) : null}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
